99a6e05828f612877ee68f88646230c02ed4e198
Sergio Robles <sergio.robles@globant.com>
2021-05-07 13:15:34 -0500

    deploy v1.1

--- src/js/login.js
+++ src/js/login.js
@@ -3,7 +3,7 @@ function login_with_google() {
     firebase.auth().signInWithRedirect(provider);
 }
 
-$(function () {
+$(() => {
     // firebase init
     let firebaseConfigBase64 = "eyJhcGlLZXkiOiJBSXphU3lEQkdKR29WdzJtM1ZJZ2tXUERJelh1Y2ZHajkzQ0ZNY2MiLCJhdXRoRG9tYWluIjoiY29sZWNjaW9uLW1rdC13ZWIuZmlyZWJhc2VhcHAuY29tIiwicHJvamVjdElkIjoiY29sZWNjaW9uLW1rdC13ZWIiLCJzdG9yYWdlQnVja2V0IjoiY29sZWNjaW9uLW1rdC13ZWIuYXBwc3BvdC5jb20iLCJtZXNzYWdpbmdTZW5kZXJJZCI6IjExNzQ1NDgwNjQxNiIsImFwcElkIjoiMToxMTc0NTQ4MDY0MTY6d2ViOmQ1Mjk5MDQwNmIwM2M4ODUyZDY3MTUiLCJtZWFzdXJlbWVudElkIjoiRy00ODZIOVRROE1HIn0=";
     let firebaseConfig = JSON.parse(atob(firebaseConfigBase64));
@@ -13,7 +13,7 @@ $(function () {
     // auth init
     firebase.auth().useDeviceLanguage();
     firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
-    firebase.auth().onAuthStateChanged(user => {
+    firebase.auth().onAuthStateChanged((user) => {
         if (user) {
             location.replace("/#collection");
         }
@@ -32,7 +32,7 @@ $(function () {
     });
 
     // events
-    $("#google_login_button").click(function (e) {
+    $("#google_login_button").click((e) => {
         login_with_google();
     });
 });
