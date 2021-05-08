c311bd355748450754ded31ef66e36dc62766aa3
Sergio Robles <srlps2702@gmail.com>
2021-05-08 01:22:12 -0500

    main view responsive

--- src/js/main.js
+++ src/js/main.js
@@ -8,17 +8,52 @@ var userContent = {
     karts: []
 };
 
+var loading_spinner = `
+<div class="row mt-5">
+    <div class="col text-center">
+        <i class="fas fa-spinner fa-5x fa-spin"></i>
+    </div>
+</div>
+`;
+
 function set_data_unsyncd() {
     idleTime = 0;
     dataSyncd = false;
-    $("#sync-icon").show();
+    $(".mkt-sync-icon").show();
 }
 
 function sync_user_data() {
     if (!dataSyncd) {
         dataSyncd = true;
-        userContentRef.set(userContent).then(() => $("#sync-icon").hide());
+        userContentRef.set(userContent).then(() => $(".mkt-sync-icon").hide());
     }
+}
+
+function addVisibilityChangeEvent(element, handler) {
+    let observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            handler(entry.intersectionRatio > 0);
+        });
+    }, {
+        root: document.documentElement
+    });
+    observer.observe(element);
+};
+
+function set_active_menu_item(item) {
+    item.addClass("active");
+    item.addClass("disabled");
+    item.attr("aria-current", "true");
+    item.attr("tabindex", "-1");
+    item.attr("aria-disabled", "true");
+}
+
+function unset_active_menu_item(item) {
+    item.removeClass("active");
+    item.removeClass("disabled");
+    item.removeAttr("aria-current");
+    item.removeAttr("tabindex");
+    item.removeAttr("aria-disabled");
 }
 
 $(() => {
@@ -36,10 +71,30 @@ $(() => {
     firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
 
     // events init
-    $("#logout-button").click((e) => {
+    $(".mkt-logout-button").click((e) => {
         firebase.auth().signOut().then(() => {
             location.replace("/login.html");
         });
+    });
+    $(".mkt-menu-item").click((e) => {
+        let item = $(e.currentTarget);
+        unset_active_menu_item($(".mkt-menu-item.active"));
+        set_active_menu_item(item);
+        switch (item.attr("href")) {
+            case "#collection":
+                $("#content-container").html(loading_spinner);
+                $("#content-container").load("/collection.html");
+                break;
+            case "#ranking":
+                $("#content-container").html(loading_spinner);
+                $("#content-container").load("/ranking.html");
+                break;
+        }
+    });
+    addVisibilityChangeEvent($("#menu-div-sm")[0], (isVisible) => {
+        if (!isVisible) {
+            $("#menuModal").modal("hide");
+        }
     });
 
     // auth init
@@ -47,8 +102,8 @@ $(() => {
         if (user) {
             // firestore read user data
             currentUser = user;
-            $("#user-display-name").text(currentUser.displayName);
-            $("#user-profile-picture").attr("src", currentUser.photoURL);
+            $(".mkt-user-display-name").text(currentUser.displayName);
+            $(".mkt-user-profile-picture").attr("src", currentUser.photoURL);
             let db = firebase.firestore();
             userContentRef = db.collection("elementos").doc(currentUser.uid);
             userContentRef.get().then((doc) => {
@@ -57,18 +112,19 @@ $(() => {
                 } else {
                     userContentRef.set(userContent);
                 }
-            });
-            // idle sync config
-            idleInterval = setInterval(() => {
-                idleTime += 1;
-                if (idleTime > 14) { // 1.5 seconds
-                    sync_user_data();
-                }
-            }, 100); // 0.1 seconds
-            // load content
-            $("#content-container").load("/collection.html", () => {
-                $("#spinner-row").hide();
-                $("#main-div").show();
+                // idle sync config
+                setInterval(() => {
+                    idleTime += 1;
+                    if (idleTime > 14) { // 1.5 seconds
+                        sync_user_data();
+                    }
+                }, 100); // 0.1 seconds
+                // load content
+                $("#content-container").load("/collection.html", () => {
+                    $("#spinner-row").hide();
+                    $("#menu-div-sm").show();
+                    $("#main-div").show();
+                });
             });
         }
         else {
